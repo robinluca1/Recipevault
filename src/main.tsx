b4dc0c1f50dc28c0df8ca258cdfd0c06389b9801
Robin Janse <robinjanse@outlook.com>
2026-06-17 10:55:56 +0200

    added basic auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,17 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+  import { Auth0Provider } from "@auth0/auth0-react";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <Auth0Provider
+    domain="dev-7mxple0m0rl4mbk6.us.auth0.com"
+    clientId="UXcD7QUZaZfHVeTzYIUDz6vAyyHGMM1n"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    >
+      <App />
+    </Auth0Provider>
+  );
   
